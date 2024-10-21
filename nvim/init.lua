c26a5a32afe0a930457808dd2a618931780067f1
pandadiestro <destructo900@gmail.com>
2024-10-20 22:30:29 -0500

    restart

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,7 @@ vim.loader.enable()
 
 --
 local disabled_built_ins = {
+    "netrw",
     "gzip",
     "zip",
     "zipPlugin",
@@ -23,12 +24,11 @@ for _, plugin in pairs(disabled_built_ins) do
 end
 --
 
-vim.opt.shadafile = "NONE"
+vim.o.shadafile = "NONE"
 vim.o.lazyredraw = true
 
 vim.o.termguicolors = true
 vim.o.number = true
-vim.o.relativenumber = true
 
 vim.o.smartindent = true
 vim.o.expandtab = true
@@ -37,7 +37,7 @@ vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 
-vim.o.backspace = "2"
+vim.o.backspace = "indent,eol,start"
 vim.o.clipboard = "unnamed,unnamedplus"
 
 vim.o.hidden = true
@@ -45,10 +45,6 @@ vim.o.mouse = "a"
 vim.o.keymodel = "startsel"
 
 vim.o.linebreak = true
-
-
--- May need for Vim (not Neovim) since coc.nvim
--- calculates byte offset by count utf-8 byte sequence
 vim.o.encoding = "utf-8"
 
 -- set to not generate backups to prevent problems
@@ -57,12 +53,14 @@ vim.o.writebackup = false
 
 -- Having longer updatetime (default is 4000 ms = 4s) leads to noticeable
 -- delays and poor user experience
-vim.o.updatetime = 400
+vim.o.updatetime = 600
 
 -- Always show the signcolumn, otherwise it would shift the text each time
 -- diagnostics appear/become resolved
 vim.o.signcolumn = "yes"
 vim.o.guicursor = "i:block"
+vim.o.cmdheight = 0
+vim.o.textwidth = 80
 
 -- Some important global variables
 vim.g.mapleader = ' '
@@ -72,6 +70,9 @@ vim.g['netrw_liststyle'] = 0
 vim.g['netrw_fastbrowse'] = 0
 vim.g['netrw_altfile'] = 0
 vim.g['netrw_banner'] = 0
+vim.g['vim_svelte_plugin_use_typescript'] = 1
+vim.g['zig_fmt_parse_errors'] = 0
+vim.g['zig_fmt_autosave'] = 0
 
 vim.api.nvim_create_autocmd("FileType", {
     pattern = 'netrw',
@@ -79,12 +80,19 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.bufhidden = 'delete'
     end
 })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = '*.mds',
+    callback = function()
+        vim.api.nvim_exec('set filetype=svelte', false)
+    end
+})
 --
 
 local globalopts = {noremap = true, silent = true}
 
 -- Basic mappings
-vim.keymap.set("i", "<C-Right>", "<C-o>e<C-o>a", globalopts)
+vim.keymap.set("i", "<C-Right>", "<Esc>ea", globalopts)
 vim.keymap.set("n", "<C-Right>", "e", globalopts)
 vim.keymap.set("x", "<C-Right>", "e", globalopts)
 
@@ -96,6 +104,10 @@ vim.keymap.set("i", "<C-Left>", "<C-o>b", globalopts)
 vim.keymap.set("n", "<C-Left>", "b", globalopts)
 vim.keymap.set("x", "<C-Left>", "b", globalopts)
 
+vim.keymap.set("i", "<C-S-Left>", "<C-o>vb", globalopts)
+vim.keymap.set("n", "<C-S-Left>", "vb", globalopts)
+vim.keymap.set("x", "<C-S-Left>", "b", globalopts)
+
 vim.keymap.set("i", "<C-z>", "<C-o>u", globalopts)
 vim.keymap.set("i", "<C-y>", "<C-o><C-r>", globalopts)
 
@@ -105,7 +117,6 @@ vim.keymap.set("x", "<BS>", "x", globalopts)
 vim.keymap.set("n", "<C-o>", ":bp<CR>", globalopts)
 vim.keymap.set("n", "<C-p>", ":bn<CR>", globalopts)
 vim.keymap.set("n", "<M-w>", ":bw<CR>", globalopts)
-vim.keymap.set("n", "<C-t>", ":Ex<CR>", globalopts)
 
 vim.keymap.set("i", "<C-BS>", "<C-w>", globalopts)
 vim.keymap.set("i", "<C-Del>", "<C-o>dw", globalopts)
@@ -118,62 +129,18 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-N>", globalopts)
 vim.keymap.set("n", "<C-d>", ":t.<CR>", globalopts)
 --
 
--- Useful commands
+-- Useful stuff
 vim.api.nvim_create_user_command('Words', '!wc -w "%"', { nargs = '?' })
 vim.api.nvim_create_user_command('JSONpretty', '%!jq .', { nargs = '?' })
---
-
--- swap lines
--- multiline movement support thanks to LunarVim
-vim.keymap.set("v", "<C-S-Up>", ":m '<-2<CR>gv=gv", globalopts)
-vim.keymap.set("v", "<C-S-Down>", ":m '>+1<CR>gv=gv", globalopts)
-
-local api = vim.api
-
-function swap_lines(n1, n2)
-    local line1 = api.nvim_buf_get_lines(0, n1 - 1, n1, false)[1]
-    local line2 = api.nvim_buf_get_lines(0, n2 - 1, n2, false)[1]
-    api.nvim_buf_set_lines(0, n1 - 1, n1, false, { line2 })
-    api.nvim_buf_set_lines(0, n2 - 1, n2, false, { line1 })
-end
-
-function swap_up()
-    local n = api.nvim_win_get_cursor(0)[1]
-    if n == 1 then
-        return
-    end
-
-    swap_lines(n, n - 1)
-    api.nvim_win_set_cursor(0, { n - 1, 0 })
-end
-
-function swap_down()
-    local n = api.nvim_win_get_cursor(0)[1]
-    local last_line = api.nvim_buf_line_count(0)
-    if n == last_line then
-        return
-    end
-
-    swap_lines(n, n + 1)
-    api.nvim_win_set_cursor(0, { n + 1, 0 })
-end
-
-vim.keymap.set("n", "<C-S-Up>", "<CMD>lua swap_up()<CR>", globalopts)
-vim.keymap.set("n", "<C-S-Down>", "<CMD>lua swap_down()<CR>", globalopts)
---
-
--- Useful stuff
-vim.fn.matchadd('ErrorMsg', [[\s\+$]]) -- detect trailing spaces
-vim.api.nvim_set_hl(0, "CocUnderline", { cterm = {underline = true}})
 --
 
 require('paq') {
     'savq/paq-nvim',                            -- paq self management
     'tpope/vim-sensible',                       -- nice defaults
-
+    'elihunter173/dirbuf.nvim',                 -- minimalistic netrw replacement
     'echasnovski/mini.starter',                 -- minimalist start screen
 
-    'neovim/nvim-lspconfig',                    -- native lsp
+    'neovim/nvim-lspconfig',                    -- lsp config block
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
@@ -181,8 +148,7 @@ require('paq') {
     'hrsh7th/nvim-cmp',
     'dcampos/nvim-snippy',
     'dcampos/cmp-snippy',
-
-    'brenton-leighton/multiple-cursors.nvim',   -- multicursors, but lua
+    'leafOfTree/vim-svelte-plugin',
 
     'kvrohit/rasmus.nvim',                      -- minimalist lua theme
     'NvChad/nvim-colorizer.lua',                -- CSS color preview
@@ -192,18 +158,35 @@ require('paq') {
     'm4xshen/autoclose.nvim',                   -- easier to configure autopairs
     'pandadiestro/nvim-markdown-preview',       -- my fork of the in-browser markdown preview plugin
     'chaoren/vim-wordmotion',                   -- better word motion for programming
-
+    {
+        'willothy/moveline.nvim',
+        build = 'make',
+    },
+    'brenton-leighton/multiple-cursors.nvim',   -- multicursors, but lua
     'nvim-lualine/lualine.nvim',                -- faster statusline
-    'echasnovski/mini.tabline',                 -- mini tab/buffer line
+    'echasnovski/mini.trailspace',              -- minimalistic trailing space solution
+  --'dstein64/vim-startuptime',                 -- benchmarking plugin
+    'jidn/vim-dbml',
 }
 
-require('multiple-cursors').setup {  }
+local moveline = require('moveline')
+vim.keymap.set("n", "<C-S-Up>", moveline.up, globalopts)
+vim.keymap.set("n", "<C-S-Down>", moveline.down, globalopts)
+vim.keymap.set("v", "<C-S-Up>", moveline.block_up, globalopts)
+vim.keymap.set("v", "<C-S-Down>", moveline.block_down, globalopts)
+
+require("dirbuf").setup {
+    hash_padding = 2,
+    show_hidden = true,
+    sort_order = "directories_first",
+    write_cmd = "DirbufSync",
+}
+
+require('multiple-cursors').setup()
 vim.keymap.set("n", "<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", globalopts)
 vim.keymap.set("n", "<C-j>", "<Cmd>MultipleCursorsAddDown<CR>", globalopts)
-
 vim.keymap.set("n", "<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", globalopts)
 vim.keymap.set("n", "<C-k>", "<Cmd>MultipleCursorsAddUp<CR>", globalopts)
-
 vim.keymap.set("x", "<C-n>", "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", globalopts)
 
 require('nvim-web-devicons').setup {
@@ -211,10 +194,10 @@ require('nvim-web-devicons').setup {
     strict = true
 }
 
-
 require('colorizer').setup()
 
-require('mini.tabline').setup()
+require('mini.trailspace').setup()
+vim.keymap.set("n", "<leader>tw", MiniTrailspace.trim, globalopts)
 
 require('autoclose').setup {
     options = {
@@ -227,7 +210,7 @@ require('autoclose').setup {
 
 require('mini.starter').setup {
     header = [[
-⠀  ⠀   (\__/)
+       (\__/)
        (•ㅅ•)          Don’t talk to
     ＿ノヽ ノ＼＿      me or my son
 `/　`/  ⌒Ｙ⌒  Ｙ ヽ     ever again.
@@ -238,22 +221,75 @@ require('mini.starter').setup {
      /ﾐ`ー―彡\  (•ㅅ•)
     / ╰    ╯  \ /    \>
 ]],
-    items = { require('mini.starter').sections.builtin_actions() },
+    items = {
+        require('mini.starter').sections.builtin_actions(),
+    },
     query_updaters = '',
     footer = [[
 "Talk is cheap. Show me the code."
                     -Torvalds]]
 }
 
+
+--- lualine setup
+local function CustomMode()
+    return '[' .. vim.fn.mode() .. ']'
+end
+
+local colors = {
+  black        = '#282828',
+  white        = '#ebdbb2',
+  red          = '#fb4934',
+  green        = '#b8bb26',
+  blue         = '#83a598',
+  yellow       = '#fe8019',
+  gray         = '#a89984',
+  darkgray     = '#3c3836',
+  lightgray    = '#504945',
+  inactivegray = '#7c6f64',
+}
+
+local CustomTheme = {
+    normal = {
+        a = { bg = colors.black, fg = colors.white, gui = 'bold' },
+        b = { bg = colors.black, fg = colors.white },
+        c = { bg = colors.black, fg = colors.white },
+    },
+    insert = {
+        a = { bg = colors.black, fg = colors.white, gui = 'bold' },
+        b = { bg = colors.black, fg = colors.white },
+        c = { bg = colors.black, fg = colors.white },
+    },
+    visual = {
+        a = { bg = colors.black, fg = colors.white, gui = 'bold' },
+        b = { bg = colors.black, fg = colors.white },
+        c = { bg = colors.black, fg = colors.white },
+    },
+    replace = {
+        a = { bg = colors.black, fg = colors.white, gui = 'bold' },
+        b = { bg = colors.black, fg = colors.white },
+        c = { bg = colors.black, fg = colors.white },
+    },
+    command = {
+        a = { bg = colors.black, fg = colors.white, gui = 'bold' },
+        b = { bg = colors.black, fg = colors.white },
+        c = { bg = colors.black, fg = colors.white },
+    },
+    inactive = {
+        a = { bg = colors.black, fg = colors.gray},
+    },
+}
+
 require('lualine').setup {
     options = {
-        disabled_filetypes = { 'netrw', 'starter' },
+        theme = CustomTheme,
+        disabled_filetypes = { 'dirbuf', 'ministarter' },
         section_separators = '',
         component_separators = '',
         globalstatus = true
     },
     sections = {
-        lualine_a = { 'mode' },
+        lualine_a = { 'fileformat', CustomMode },
         lualine_b = {
             'branch',
             'diff',
@@ -266,18 +302,49 @@ require('lualine').setup {
                 colored = false
             }
         },
-        lualine_c = { '%=' , 'filename'},
-        lualine_y = {}
+        lualine_c = {
+            '%=',
+            {
+                'filename',
+                path = 1,
+            },
+        },
+        lualine_x = { 'encoding', 'filetype' },
+        lualine_y = { 'location' },
+        lualine_z = {},
+    },
+    tabline = {
+        lualine_a = {
+            {
+                'buffers',
+                symbols = {
+                    modified = ' +',  -- Text to show when the file is modified
+                    alternate_file = '', -- Text to show to identify the alternate file
+                },
+            },
+        },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {
+            {
+                'tabs',
+                symbols = {
+                    modified = ' +',  -- Text to show when the file is modified
+                    alternate_file = '', -- Text to show to identify the alternate file
+                },
+            }
+        },
     },
 }
+---
 
-
--- rasmus configuration
+-- rasmus colorscheme configuration
 vim.g.rasmus_italic_comments = false
 vim.g.rasmus_transparent = true
-
-
 vim.api.nvim_exec('colorscheme rasmus', false)
+
 vim.api.nvim_set_hl(0, "ErrorMsg", {
     fg = "#FF7070",
     underline = true,
@@ -300,9 +367,7 @@ vim.api.nvim_set_hl(0, "MiniTablineModifiedVisible", {
 vim.api.nvim_set_hl(0, "MiniTablineModifiedHidden", {
     italic = true,
 })
-
 --
-
 
 
 -- LSP config
@@ -311,8 +376,8 @@ vim.api.nvim_set_hl(0, "MiniTablineModifiedHidden", {
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<leader>f', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
@@ -322,6 +387,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(ev)
         -- Enable completion triggered by <c-x><c-o>
         vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        -- I just want my colorscheme to look the way it was intended
+        --local bufnr = ev.buf
+        --local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        --client.server_capabilities.semanticTokensProvider = nil
 
         -- Buffer local mappings.
         -- See `:help vim.lsp.*` for documentation on any of the below functions
@@ -350,10 +420,12 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 vim.diagnostic.config({
-    virtual_text = false,
+    virtual_text = true,
     update_in_insert = true,
     signs = true,
-    float = { border = "single" },
+    float = {
+        border = "solid",
+    },
 })
 
 vim.api.nvim_create_autocmd('CursorHold', {
@@ -439,29 +511,26 @@ require('cmp').setup.cmdline(':', {
     }
 })
 
-
 -- Lspconfig capabilities
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
 
+lspconfig['typst_lsp'].setup{
+    settings = {
+        exportPdf = "onSave",
+    },
+}
+
 lspconfig['ccls'].setup {
     capabilities = capabilities,
     init_options = {
-        compilationDatabaseDirectory = "build",
-        index = {
-            threads = 0;
-        },
-        clang = {
-            excludeArgs = { "-frounding-math" },
-        },
         cache = {
-            directory = '/tmp/ccls',
+            directory = "/tmp/ccls-cache",
         },
     },
 }
 
-lspconfig['gopls'].setup {
-    cmd = {'gopls'},
+lspconfig['gopls'].setup{
     capabilities = capabilities,
     settings = {
         gopls = {
@@ -478,8 +547,24 @@ lspconfig['gopls'].setup {
     },
 }
 
-lspconfig['tsserver'].setup {}
 
+lspconfig['ts_ls'].setup {
+    capabilities = capabilities,
+}
 
+lspconfig['svelte'].setup {
+    capabilities = capabilities,
+}
 
+lspconfig['cssls'].setup {
+    capabilities = capabilities,
+}
+
+lspconfig['arduino_language_server'].setup{
+    capabilities = capabilities,
+}
+
+lspconfig['zls'].setup{
+    capabilities = capabilities,
+}
 
